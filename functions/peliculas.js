2782e89ae045e4de6865a34d9f016c865c666a6f
sniperdes <sniperdes898@gmail.com>
2026-06-27 02:06:24 -0300

    Update access token in peliculas.js

--- functions/peliculas.js
+++ functions/peliculas.js
@@ -1,5 +1,5 @@
 export async function onRequest() {
-  const token = "TU_ACCESS_TOKEN_REAL_DE_TMDB_AQUÍ";
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhY2Q2NTM0MmI5ODZmZjdkYWQ5MDJlYTc0MTJmYzAwMyIsIm5iZiI6MTc4MjQ4MDg3Mi45Njk5OTk4LCJzdWIiOiI2YTNlN2ZlODkwYjQ2NjA4ZmUzYjViMzgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Et-PdA6n_O2ivehKH1nwUUXdhUG5dE6WgaEVjL1Iqm4";
   const titulos = ['Mad Max: Fury Road', 'Gladiador', 'Misión Imposible', 'Terminator 2', 'Batman: El Caballero', 'Duro de Matar', 'Matrix', 'Top Gun: Maverick', '300', 'Deadpool', 'Logan', 'The Avengers'];
   let lista = [];
 
