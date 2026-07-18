0375c8df8823c51898c2e7eaf3bd046b25421cfb
sniperdes <sniperdes898@gmail.com>
2026-07-18 20:44:45 -0300

    Add translation for 'Contra el destino'

--- functions/bot.js
+++ functions/bot.js
@@ -20,6 +20,7 @@ const SERIES_CONOCIDAS = {
     'asi': 'asi',
     'aşı': 'asi',
     'elif': 'elif',
+    'Contra el destino': 'Kadere Karşı',
     'dirilis ertugrul': 'dirilis-ertugrul',
     'diriliş ertuğrul': 'dirilis-ertugrul',
     'resurrection ertugrul': 'dirilis-ertugrul',
