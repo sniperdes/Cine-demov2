5e985f5e21bde36062c9ffcbfd76fe742dacdb36
sniperdes <sniperdes898@gmail.com>
2026-07-20 16:25:08 -0300

    Add 'nombreKV' field to 'Rancho Dutton' entry

--- data-series.js
+++ data-series.js
@@ -1,7 +1,7 @@
 // ─── BASE DE DATOS SERIES ─────────────────────────────────────────────────
         const baseDeDatosSeries = [
             // DRAMA
-            { titulo:'Rancho Dutton',       tmdbQuery:'Rancho Dutton 2026',      genero:'drama',        info:'⭐ 9.2 | 🎭 Drama | 📺 1 temporadas',   desc:'Rip y Beth Dutton están construyendo una nueva vida en el sur de Texas junto al joven Carter, pero la promesa de un futuro lejos de Yellowstone choca con duras realidades y con un rancho rival que no se detendrá ante nada para proteger su imperio.' },
+            { titulo:'Rancho Dutton',       tmdbQuery:'Rancho Dutton 2026',   nombreKV:'rancho-dutton',   genero:'drama',        info:'⭐ 9.2 | 🎭 Drama | 📺 1 temporadas',   desc:'Rip y Beth Dutton están construyendo una nueva vida en el sur de Texas junto al joven Carter, pero la promesa de un futuro lejos de Yellowstone choca con duras realidades y con un rancho rival que no se detendrá ante nada para proteger su imperio.' },
             // COMEDIA
             { titulo:'The Boys',           tmdbQuery:'The Boys 2019',          genero:'comedia',      info:'⭐ 8.7 | 🎭 Acción | 📺 4 temporadas',   desc:'Superhéroes corruptos enfrentados por un grupo de vigilantes.' },
             // ACCIÓN
