4315cb09f4aae43479fc69eb9959c6a0b6f4aea9
sniperdes <sniperdes898@gmail.com>
2026-07-30 12:55:49 -0300

    Replace 'Reply 1988' with 'Así aprenderás'

--- data-doramas.js
+++ data-doramas.js
@@ -3,7 +3,7 @@
             // ROMANCE
             { titulo:'Crash Landing on You',   tmdbQuery:'Crash Landing on You 2019',  genero:'dorama-romance', info:'⭐ 8.7 | 🌸 Dorama | 📺 16 ep', desc:'Una heredera surcoreana aterriza accidentalmente en Corea del Norte.' },
             // DRAMA
-            { titulo:'Reply 1988',              tmdbQuery:'Reply 1988 2015',            genero:'dorama-drama', info:'⭐ 9.2 | 🌸 Dorama | 📺 20 ep', desc:'Cinco familias vecinas en el Seúl de 1988.' },
+            { titulo:'Así aprenderás', tmdbQuery:'참교육 2026', nombreKV:'así-aprenderás', genero:'drama-accion', info:'⭐ 9.4 | 📺', desc:'Unos inspectores insólitos entran en escena en los colegios, con lecciones duras de las que no aparecen en los libros de texto, para acabar con las reiteradas f' },
             // ACCIÓN
             { titulo:'Vagabond',                tmdbQuery:'Vagabond 2019',              genero:'dorama-accion', info:'⭐ 8.1 | 🌸 Dorama | 📺 16 ep', desc:'Un hombre investiga un accidente de avión que mató a su sobrino.' },
             ];
