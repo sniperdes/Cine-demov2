1f0f3c092dcfc8ba6773f5c3f84867e787a1b5cc
sniperdes <sniperdes898@gmail.com>
2026-07-30 13:03:43 -0300

    Change genre of 'Así aprenderás' to 'dorama-drama'

--- data-doramas.js
+++ data-doramas.js
@@ -3,7 +3,7 @@
             // ROMANCE
             { titulo:'Crash Landing on You',   tmdbQuery:'Crash Landing on You 2019',  genero:'dorama-romance', info:'⭐ 8.7 | 🌸 Dorama | 📺 16 ep', desc:'Una heredera surcoreana aterriza accidentalmente en Corea del Norte.' },
             // DRAMA
-            { titulo:'Así aprenderás', tmdbQuery:'참교육 2026', nombreKV:'así-aprenderás', genero:'dorama-accion', info:'⭐ 9.4 | 📺', desc:'Unos inspectores insólitos entran en escena en los colegios, con lecciones duras de las que no aparecen en los libros de texto, para acabar con las reiteradas  faltas de respeto.' },
+            { titulo:'Así aprenderás', tmdbQuery:'참교육 2026', nombreKV:'así-aprenderás', genero:'dorama-drama', info:'⭐ 9.4 | 📺', desc:'Unos inspectores insólitos entran en escena en los colegios, con lecciones duras de las que no aparecen en los libros de texto, para acabar con las reiteradas  faltas de respeto.' },
             // ACCIÓN
             { titulo:'Vagabond',                tmdbQuery:'Vagabond 2019',              genero:'dorama-accion', info:'⭐ 8.1 | 🌸 Dorama | 📺 16 ep', desc:'Un hombre investiga un accidente de avión que mató a su sobrino.' },
             ];
