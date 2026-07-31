798bcc1b50ba5cfd793defe182246958ff4fd9e1
sniperdes <sniperdes898@gmail.com>
2026-07-31 13:10:04 -0300

    Update 'Abogado de oficio' tmdbQuery format

--- data-doramas.js
+++ data-doramas.js
@@ -4,7 +4,7 @@
             { titulo:'Crash Landing on You',   tmdbQuery:'Crash Landing on You 2019',  genero:'dorama-romance', info:'⭐ 8.7 | 🌸 Dorama | 📺 16 ep', desc:'Una heredera surcoreana aterriza accidentalmente en Corea del Norte.' },
             // DRAMA
             { titulo:'Así aprenderás', tmdbQuery:'참교육 2026', nombreKV:'así-aprenderás',  genero:'dorama-drama', info:'⭐ 9.4 | 📺', desc:'Unos inspectores insólitos entran en escena en los colegios, con lecciones duras de las que no aparecen en los libros de texto, para acabar con las reiteradas  faltas de respeto.' },
-            { titulo:'Abogado de oficio', tmdbQuery:'Abogado de oficio 2025', nombreKV:'pro-bono',   genero:'dorama-drama ', info:'⭐ 8.3 | 📺', desc:'Un juez insigne cuya vida cambia de la noche a la mañana se suma al equipo de asistencia jurídica gratuita en un selecto bufete  Allí, la esperanza aflora en lugares insospechados.' },
+            { titulo:'Abogado de oficio', tmdbQuery:'Abogado de oficio (2025)', nombreKV:'pro-bono',   genero:'dorama-drama ', info:'⭐ 8.3 | 📺', desc:'Un juez insigne cuya vida cambia de la noche a la mañana se suma al equipo de asistencia jurídica gratuita en un selecto bufete  Allí, la esperanza aflora en lugares insospechados.' },
             // ACCIÓN
             { titulo:'Vagabond',                tmdbQuery:'Vagabond 2019',              genero:'dorama-accion', info:'⭐ 8.1 | 🌸 Dorama | 📺 16 ep', desc:'Un hombre investiga un accidente de avión que mató a su sobrino.' },
             ];
