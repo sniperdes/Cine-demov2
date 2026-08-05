a11cf47110e28d3d4078911d13022e9e8a96790f
sniperdes <sniperdes898@gmail.com>
2026-08-05 16:38:28 -0300

    Add new romance dorama entry to the database

--- data-doramas.js
+++ data-doramas.js
@@ -3,7 +3,8 @@
             // ROMANCE
             { titulo:'Nos vemos en la oficina', tmdbQuery:'내일도 출근! 2026', nombreKV:'nos-vemos-en-la-oficina', genero:'dorama-romance', info:'⭐ 9.0 | 📺', desc:'Cha Ji-yoon lleva 7 años trabajando en el mundo corporativo y está cansada del trabajo y de las citas. Kang Si-woo, un cínico jefe con mala reputación, se cruza.' },
             { titulo:'Un sueño contigo', tmdbQuery:'그대에게 드림 2026', nombreKV:'un-sueño-contigo', genero:'dorama-romance', info:'⭐ 7.9 | 📺', desc:'Sin sinopsis disponible.' },
-            // DRAMA
+            { titulo:'Novio a la carta', tmdbQuery:'월간남친 2026', nombreKV:'un-novio-por-suscripción', genero:'dorama-romance', info:'⭐ 8.3 | 📺', desc:'Seo Mi-rae vive absorbida por el trabajo —sin tiempo para el amor— hasta que un servicio de citas despierta sentimientos que creía dormidos y abre la puerta.' },
+           // DRAMA
             { titulo:'Así aprenderás', tmdbQuery:'참교육 2026', nombreKV:'así-aprenderás',  genero:'dorama-drama', info:'⭐ 9.4 | 📺', desc:'Unos inspectores insólitos entran en escena en los colegios, con lecciones duras de las que no aparecen en los libros de texto, para acabar con las reiteradas  faltas de respeto.' },
             { titulo:'Pro bono ', tmdbQuery:'Abogado de oficio 2025', nombreKV:'pro-bono',   genero:'dorama-drama', info:'⭐ 8.3 | 📺', desc:'Un juez insigne cuya vida cambia de la noche a la mañana se suma al equipo de asistencia jurídica gratuita en un selecto bufete  Allí, la esperanza aflora en lugares insospechados.' },
             // ACCIÓN
