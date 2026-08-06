d6cfc78709830c95f3c8ba1eb64eba756639ac2c
sniperdes <sniperdes898@gmail.com>
2026-08-06 10:21:58 -0300

    Remove Jujutsu Kaisen 0 entry from anime database

--- data-anime.js
+++ data-anime.js
@@ -1,5 +1,4 @@
 // ─── BASE DE DATOS ANIME ──────────────────────────────────────────────────
         const baseDeDatosAnime = [
             { titulo:'Ore dake Level Up na Ken Solo Leveling ',           tmdbQuery:'solo leveling 2024',    nombreKV:'solo-leveling',      info:'⭐ 9.0 | 🎌 Anime | 📺 2 temporadas',   desc:'Lo que no te mata te hace más fuerte, pero en el caso de Sung Jinwoo, lo que lo mató lo hizo más fuerte. Después de ser brutalmente asesinado por monstruos en una mazmorra de alto rango, Jinwoo regresó con el Sistema, un programa que solo él puede ver y que eleva su nivel en todos los sentidos. Ahora, está decidido a descubrir los secretos detrás de sus poderes y la mazmorra que los engendró.' },
-            { titulo:'Jujutsu Kaisen 0 ',                                  tmdbQuery:'Jujutsu Kaisen 0 2021',  nombreKV:'jujutsu-kaisen-0',     info:'⭐ 8.1 | 🎌 Anime| 📺', desc:'Yuta Okkotsu es un nervioso estudiante de instituto que sufre un grave problema: su amiga de la infancia Rika se ha convertido en una maldición y no le deja en.' },
         ];
