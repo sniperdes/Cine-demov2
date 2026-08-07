a7d4c3dd19805394041e79feb7552950d2fb5de9
sniperdes <sniperdes898@gmail.com>
2026-08-07 04:19:25 -0300

    Add new entry for 'El genio y los deseos'

--- functions/bot.js
+++ functions/bot.js
@@ -59,6 +59,7 @@ const SERIES_CONOCIDAS = {
     'Un novio por suscripción': 'un-novio-por-suscripción',
     'Hacemos lo que podemos': 'hacemos-lo-que-podemos',
     'En tu mejor momento': 'en-tu-mejor-momento',
+    'El genio y los deseos': 'el-genio-y-los-deseos',
     // ── Anime (van a data-anime.js) ─────────────────────────────────
     'Ore dake Level Up na Ken Solo Leveling ': 'solo-leveling',
     'Solo Leveling': 'solo-leveling',
