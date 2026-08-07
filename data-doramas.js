f4b3acda77734d4e78e2dd12f3b533d2d6869597
sniperdes <sniperdes898@gmail.com>
2026-08-07 04:14:25 -0300

    Reorganize dorama entries and add new drama titles

--- data-doramas.js
+++ data-doramas.js
@@ -4,11 +4,13 @@
             { titulo:'Nos vemos en la oficina', tmdbQuery:'내일도 출근! 2026', nombreKV:'nos-vemos-en-la-oficina', genero:'dorama-romance', info:'⭐ 9.0 | 📺', desc:'Cha Ji-yoon lleva 7 años trabajando en el mundo corporativo y está cansada del trabajo y de las citas. Kang Si-woo, un cínico jefe con mala reputación, se cruza.' },
             { titulo:'Un sueño contigo', tmdbQuery:'그대에게 드림 2026', nombreKV:'un-sueño-contigo', genero:'dorama-romance', info:'⭐ 7.9 | 📺', desc:'Sin sinopsis disponible.' },
             { titulo:'Novio a la carta', tmdbQuery:'월간남친 2026', nombreKV:'un-novio-por-suscripción', genero:'dorama-romance', info:'⭐ 8.3 | 📺', desc:'Seo Mi-rae vive absorbida por el trabajo —sin tiempo para el amor— hasta que un servicio de citas despierta sentimientos que creía dormidos y abre la puerta.' },
-           // DRAMA
+            
+            // DRAMA
             { titulo:'Hacemos lo que podemos', tmdbQuery:'모두가 자신의 무가치함과 싸우고 있다 2026', nombreKV:'hacemos-lo-que-podemos', genero:'dorama-drama', info:'⭐ 8.5 | 📺', desc:'Tras 20 años tratando de hacer su primera película, un aspirante a director toca fondo. Pero entonces conoce a una productora saturada que le ayuda a volver.' },
             { titulo:'Así aprenderás', tmdbQuery:'참교육 2026', nombreKV:'así-aprenderás',  genero:'dorama-drama', info:'⭐ 9.4 | 📺', desc:'Unos inspectores insólitos entran en escena en los colegios, con lecciones duras de las que no aparecen en los libros de texto, para acabar con las reiteradas  faltas de respeto.' },
             { titulo:'Pro bono ', tmdbQuery:'Abogado de oficio 2025', nombreKV:'pro-bono',   genero:'dorama-drama', info:'⭐ 8.3 | 📺', desc:'Un juez insigne cuya vida cambia de la noche a la mañana se suma al equipo de asistencia jurídica gratuita en un selecto bufete  Allí, la esperanza aflora en lugares insospechados.' },
             { titulo:'En tu mejor momento', tmdbQuery:'In Your Radiant Season 2026', nombreKV:'en-tu-mejor-momento', genero:'dorama-drama', info:'⭐ 8.1 | 📺', desc:'Song Ha Ran es la diseñadora jefe de Nana Atelier, la principal casa de moda de alta gama de Corea A diferencia de su imagen impecable y aparentemente perfecta.' },
+            { titulo:'El genio y los deseos', tmdbQuery:'다 이루어질지니 2025', nombreKV:'el-genio-y-los-deseos', genero:'dorama-drama', info:'⭐ 7.9 | 📺', desc:'Un excéntrico genio reaparece después de mil años para cumplir los deseos de una mujer impasible Logrará colmarla de amor e ilusión con su magia.' },
             // ACCIÓN
             { titulo:'Vagabond',                tmdbQuery:'Vagabond 2019',              genero:'dorama-accion', info:'⭐ 8.1 | 🌸 Dorama | 📺 16 ep', desc:'Un hombre investiga un accidente de avión que mató a su sobrino.' },
             ];
