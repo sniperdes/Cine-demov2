ba8ace41d0d7591dfefaca91924f5ad6b9d941d6
sniperdes <sniperdes898@gmail.com>
2026-08-07 23:23:33 -0300

    Add new dorama 'El capo del bloque' to data

--- data-doramas.js
+++ data-doramas.js
@@ -16,5 +16,6 @@
             // ACCIÓN
             { titulo:'Un chico ejemplar', tmdbQuery:'Un chico ejemplar 2025', nombreKV:'un-chico-ejemplar', genero:'dorama-accion', info:'⭐ 8.7 | 📺', desc:'Tras 11 años, la Policía de Insung acoge a antiguos deportistas de la selección nacional Estos medallistas que fueron héroes se enfrentan hoy a una cruda realidad.' },
             { titulo:'Perros de caza', tmdbQuery:'사냥개들 2023', nombreKV:'perros-de-caza', genero:'dorama-accion', info:'⭐ 8.5 | 📺', desc:'Dos jóvenes boxeadores se alían con un prestamista benévolo para acabar con un despiadado usurero que se aprovecha de los más endeudados y desesperados.' },
+            { titulo:'El capo del bloque', tmdbQuery:'아파트 2026', nombreKV:'el-capo-del-bloque', genero:'dorama-accion', info:'⭐ 8.7 | 📺', desc:'Un capo se alía con una abogada en ciernes para robar el fondo de reserva de un complejo de apartamentos y acaban destapando una red de corrupción.' },
 
             ];
