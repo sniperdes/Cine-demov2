a2bcdfa5cb1561b6b5d0842c980104a13993eeb3
sniperdes <sniperdes898@gmail.com>
2026-08-12 00:18:14 -0300

    Add new dorama 'Acaramelados' to the list
    Added a new dorama titled 'Acaramelados' with details including tmdbQuery, nombreKV, genero, info, and desc.

--- data-doramas.js
+++ data-doramas.js
@@ -4,7 +4,8 @@
             { titulo:'Nos vemos en la oficina', tmdbQuery:'내일도 출근! 2026', nombreKV:'nos-vemos-en-la-oficina', genero:'dorama-romance', info:'⭐ 9.0 | 📺', desc:'Cha Ji-yoon lleva 7 años trabajando en el mundo corporativo y está cansada del trabajo y de las citas. Kang Si-woo, un cínico jefe con mala reputación, se cruza.' },
             { titulo:'Un sueño contigo', tmdbQuery:'그대에게 드림 2026', nombreKV:'un-sueño-contigo', genero:'dorama-romance', info:'⭐ 7.9 | 📺', desc:'Sin sinopsis disponible.' },
             { titulo:'Novio a la carta', tmdbQuery:'월간남친 2026', nombreKV:'un-novio-por-suscripción', genero:'dorama-romance', info:'⭐ 8.3 | 📺', desc:'Seo Mi-rae vive absorbida por el trabajo —sin tiempo para el amor— hasta que un servicio de citas despierta sentimientos que creía dormidos y abre la puerta.' },
-            
+            { titulo:'Acaramelados', tmdbQuery:'이런 엿같은 사랑 2026', nombreKV:'acaramelados', genero:'dorama-romance', info:'⭐ 7.8 | 📺', desc:'Una fiscal ambiciosa pierde la memoria y acaba conviviendo con un entrenador de boxeo que asegura ser su novio. ¿Puede el amor verdadero nacer de una mentira?' },
+
             // DRAMA
             { titulo:'Hacemos lo que podemos', tmdbQuery:'모두가 자신의 무가치함과 싸우고 있다 2026', nombreKV:'hacemos-lo-que-podemos', genero:'dorama-drama', info:'⭐ 8.5 | 📺', desc:'Tras 20 años tratando de hacer su primera película, un aspirante a director toca fondo. Pero entonces conoce a una productora saturada que le ayuda a volver.' },
             { titulo:'Así aprenderás', tmdbQuery:'참교육 2026', nombreKV:'así-aprenderás',  genero:'dorama-drama', info:'⭐ 9.4 | 📺', desc:'Unos inspectores insólitos entran en escena en los colegios, con lecciones duras de las que no aparecen en los libros de texto, para acabar con las reiteradas  faltas de respeto.' },
@@ -12,7 +13,7 @@
             { titulo:'En tu mejor momento', tmdbQuery:'In Your Radiant Season 2026', nombreKV:'en-tu-mejor-momento', genero:'dorama-drama', info:'⭐ 8.1 | 📺', desc:'Song Ha Ran es la diseñadora jefe de Nana Atelier, la principal casa de moda de alta gama de Corea A diferencia de su imagen impecable y aparentemente perfecta.' },
             { titulo:'El genio y los deseos', tmdbQuery:'다 이루어질지니 2025', nombreKV:'el-genio-y-los-deseos', genero:'dorama-drama', info:'⭐ 7.9 | 📺', desc:'Un excéntrico genio reaparece después de mil años para cumplir los deseos de una mujer impasible Logrará colmarla de amor e ilusión con su magia.' },
             { titulo:'GOBLIN El solitario ser inmortal', tmdbQuery:'GOBLIN: El solitario ser inmortal 2016', nombreKV:'goblin-el-solitario-ser-inmortal', genero:'dorama-drama', info:'⭐ 8.6 | 📺', desc:'Un ser inmortal desea terminar su existencia buscando en todo el mundo su novia que es la destinada a sacar la espada que le atraviesa el pecho y solo ella es.' },
-
+            
             // ACCIÓN
             { titulo:'Un chico ejemplar', tmdbQuery:'Un chico ejemplar 2025', nombreKV:'un-chico-ejemplar', genero:'dorama-accion', info:'⭐ 8.7 | 📺', desc:'Tras 11 años, la Policía de Insung acoge a antiguos deportistas de la selección nacional Estos medallistas que fueron héroes se enfrentan hoy a una cruda realidad.' },
             { titulo:'Perros de caza', tmdbQuery:'사냥개들 2023', nombreKV:'perros-de-caza', genero:'dorama-accion', info:'⭐ 8.5 | 📺', desc:'Dos jóvenes boxeadores se alían con un prestamista benévolo para acabar con un despiadado usurero que se aprovecha de los más endeudados y desesperados.' },
